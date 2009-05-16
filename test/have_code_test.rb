87c286df8c367b908dfe42102110949041bb96b7
Bryce Kerley <bkerley@brycekerley.net>
2009-05-16 11:15:26 -0400

    copy ar test into dm test

--- test/have_code_test.rb
+++ test/have_code_test.rb
@@ -25,5 +25,20 @@ class HaveCodeTest < Test::Unit::TestCase
     should "have a 'have_code' method" do
       assert Goblet.respond_to?(:have_code)
     end
+    context 'when configured with a code' do
+      setup do
+        Goblet.have_code(60466169, 12034710206, 81268112)
+      end
+      should "have a 'find_by_code' method" do
+        assert Goblet.respond_to?(:find_by_code)
+      end
+      should 'create an object, get its code, and recall it by code' do
+        kohg = Goblet.create :name=>'king of hyrule', :material=>'gold'
+        code = kohg.code
+        assert code.is_a?(String)
+        zelda = Goblet.find_by_code code
+        assert_equal kohg, zelda
+      end
+    end
   end
 end
