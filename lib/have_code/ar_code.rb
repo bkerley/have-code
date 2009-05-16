c50737e34c0b9e21e254c7fd58c7fd982e083db4
Bryce Kerley <bkerley@brycekerley.net>
2009-05-16 11:03:48 -0400

    get closer to passing scenario

--- lib/have_code/ar_code.rb
+++ lib/have_code/ar_code.rb
@@ -6,20 +6,26 @@ module HaveCode
 
     module ClassMethods
       def have_code(mod, a, b)
-        @@have_code_affine = Affine::Cipher.new(mod, a, b)
+        write_inheritable_attribute :have_code_cipher, Affine::Cipher.new(mod, a, b)
 
         extend OptionalClassMethods
+        include InstanceMethods
       end
     end
 
     module OptionalClassMethods
-      def find_by_code
+      def find_by_code(id)
+
       end
     end
 
     module InstanceMethods
+      def have_code_cipher
+        self.class.read_inheritable_attribute :have_code_cipher
+      end
+
       def code
-        @@have_code_affine.encrypt(id).to_s(36)
+        have_code_cipher.encipher(id).to_s(36)
       end
     end
   end
