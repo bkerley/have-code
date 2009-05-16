218bb6b58adbaa2673f8512846731d69adfaa30a
Bryce Kerley <bkerley@brycekerley.net>
2009-05-16 10:40:51 -0400

    pass find_by_code presence test

--- lib/have_code/ar_code.rb
+++ lib/have_code/ar_code.rb
@@ -5,7 +5,21 @@ module HaveCode
     end
 
     module ClassMethods
-      def have_code
+      def have_code(mod, a, b)
+        @@have_code_affine = Affine::Cipher.new(mod, a, b)
+
+        extend OptionalClassMethods
+      end
+    end
+
+    module OptionalClassMethods
+      def find_by_code
+      end
+    end
+
+    module InstanceMethods
+      def code
+        @@have_code_affine.encrypt(id).to_s(36)
       end
     end
   end
