02c97413aa584214f7e090d4dd035eb36f961830
Bryce Kerley <bkerley@brycekerley.net>
2009-05-16 10:34:00 -0400

    create table

--- test/active_record_helper.rb
+++ test/active_record_helper.rb
@@ -1,5 +1,9 @@
 require 'active_record'
 HaveCode.enable_activerecord
-ActiveRecord::Base.establish_connection(:adapter => "sqlite", :database => ":memory:")
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+ActiveRecord::Base.connection.create_table :mugs do |t|
+  t.column :name, :string
+  t.column :material, :string
+end
 class Mug < ActiveRecord::Base
 end
