53741fe52494737cc7818509a9bebc755b24e625
Bryce Kerley <bkerley@brycekerley.net>
2009-05-16 11:15:15 -0400

    add columns to the goblet

--- test/data_mapper_helper.rb
+++ test/data_mapper_helper.rb
@@ -3,4 +3,8 @@ HaveCode.enable_datamapper
 DataMapper.setup(:default, 'sqlite3::memory:')
 class Goblet
   include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :material, String
 end
+DataMapper.auto_upgrade!
