7d4c5accf6c1c55b16e498224db179ebb24c0757
Bryce Kerley <bkerley@brycekerley.net>
2009-05-16 10:34:11 -0400

    failing initialization tests

--- test/have_code_test.rb
+++ test/have_code_test.rb
@@ -5,6 +5,14 @@ class HaveCodeTest < Test::Unit::TestCase
     should "have a 'have_code' method" do
       assert Mug.respond_to?(:have_code)
     end
+    context "when configured with a code" do
+      setup do
+        Mug.have_code(60466169, 12034710206, 81268112)
+      end
+      should "have a 'find_by_code' method" do
+        assert Mug.respond_to?(:find_by_code)
+      end
+    end
   end
   in_datamapper do
     should "have a 'have_code' method" do
